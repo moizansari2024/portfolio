3e65e5fec92e351fc72e4fa285a17610fe83945e
moizansari2024 <moizansari672@gmail.com>
2025-02-12 13:31:07 -0800

    portfolio

---
+++ scrpit.js
@@ -0,0 +1,15 @@
+const nameElement = document.getElementById("typing-name");
+const nameText = "Moiz Ahmed";
+let index = 0;
+
+function typeEffect() {
+    nameElement.innerHTML = nameText.substring(0, index);
+    index++;
+
+    if (index > nameText.length) {
+        index = 0;  // Restart loop
+    }
+    setTimeout(typeEffect, 200);
+}
+
+typeEffect();
